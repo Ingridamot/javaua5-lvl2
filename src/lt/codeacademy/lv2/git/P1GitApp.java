2fb2ed28c84cc9fd90ca58eaa9ace7c7229628e1
Ingrida <motuziene47@gmail.com>
2023-10-17 18:43:34 +0300

    add new StudentDto.java if else with boolean

--- src/lt/codeacademy/lv2/git/P1GitApp.java
+++ src/lt/codeacademy/lv2/git/P1GitApp.java
@@ -4,6 +4,17 @@ public class P1GitApp {
     public static void main(String[] args) {
 
         System.out.println("hello");
+        ifExample()
 
+    }
+
+    private static void ifExample() {
+        boolean tiesa = (5>3);
+
+        if (5>3){
+            tiesa =true;
+        } else {
+            tiesa = false;
+        }
     }
 }
